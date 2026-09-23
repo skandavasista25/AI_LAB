230782ad24618cf3683695092df265a8eff05087
Skanda Vasista <skandavasista.cs24@bmsce.ac.in>
2026-09-23 13:16:22 +0530

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,11 @@
+n = int(input("Enter number of elements: "))
+arr = []
+for i in range(n):
+    arr.append(int(input("Enter element: ")))
+for i in range(n):
+    min_index = i
+    for j in range(i + 1, n):
+        if arr[j] < arr[min_index]:
+            min_index = j
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+print("Sorted array:", arr)
